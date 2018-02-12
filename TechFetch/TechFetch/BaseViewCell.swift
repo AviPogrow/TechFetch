45da827e8081df4441f912d421b5b1ce46ab57d8
Yun Park <34497076+yunjoonpark@users.noreply.github.com>
2018-02-12 15:11:33 -0500

    Added pop up cell near bottom of screen when the 'Source' button is clicked.

---
+++ TechFetch/TechFetch/BaseViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewCell.swift
+//  TechFetch
+//
+//  Created by Jason Park on 2/12/18.
+//  Copyright © 2018 Jason Park. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TechFetch/TechFetch/Menu.swift
+++ TechFetch/TechFetch/Menu.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class Menu {
+class Menu: NSObject, UITableViewDelegate, UITableViewDataSource {
     
     let darkView = UIView()
+    let menuTableView = UITableView()
+    let arrayOfSources = ["TechCrunch", "TechRadar"]
     
     public func openMenu() {
         if let window = UIApplication.shared.keyWindow {
@@ -19,10 +21,18 @@ class Menu {
             
             darkView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.closeMenu)))
             
+            let height: CGFloat = 100
+            
+            let y = window.frame.height - height
+            
+            menuTableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
             window.addSubview(darkView)
+            window.addSubview(menuTableView)
             
             UIView.animate(withDuration: 0.4, animations: {
                 self.darkView.alpha = 1
+                self.menuTableView.frame.origin.y = y
             })
         }
     }
@@ -30,7 +40,45 @@ class Menu {
     @objc public func closeMenu() {
         UIView.animate(withDuration: 0.4, animations: {
             self.darkView.alpha = 0
+            if let window = UIApplication.shared.keyWindow {
+                self.menuTableView.frame.origin.y = window.frame.height
+            }
         })
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfSources.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = arrayOfSources[indexPath.item]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    
+    override init() {
+        super.init()
+        
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        
+        menuTableView.isScrollEnabled = false
+        menuTableView.bounces = false
+        
+        menuTableView.register(BaseViewCell.classForCoder(), forCellReuseIdentifier: "cellId")
     }
     
     
